68c86381608b545ca382ee7a770a1a5d13f7025d
Lightbender255 <lightbender255@gmail.com>
2025-12-03 23:14:31 -0600

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   root: 'src/web',
+  envDir: path.resolve(__dirname, '.'),
   server: {
     port: 5173,
     strictPort: false
